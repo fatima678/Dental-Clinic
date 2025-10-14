540296ebd96d8fc30ffe216b4f270e4f2dc4a5c0
Fatima <fatimatulzahra7802@gmail.com>
2025-10-14 14:41:15 -0700

    Implement multi-step appointment booking flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Services from './components/Services';
+import About from './components/About';
+import Tips from './components/Tips';
+import BookAppointment from './components/BookAppointment';
 
 function App() {
   return (
    <div>
     <Header/>
     <Hero/>
+    <Services/>
+    <About/>
+    <Tips/>
+    <BookAppointment/>
    </div>
   );
 }
